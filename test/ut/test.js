31b94e1eb483cd917e3bbb2f0a51a41f67df1ab5
xiangshouding <fansekey@gmail.com>
2014-07-08 18:53:02 +0800

    update ut

--- test/ut/test.js
+++ test/ut/test.js
@@ -1,29 +1,54 @@
-var Swig = require('../../');
-var rApi = require('../../../yog/middleware/yog-resource-api');
-var fs = require('fs');
-var r = {
-    fis: new rApi.ResourceApi(__dirname + '/config'),
-    write: function (s) {
-        console.log(s);
-    }
-}
+var request = require('supertest');
+var express = require('express');
+var expect = require('chai').expect;
+var yog = require('yog');
+var app = express();
+var path = require('path');
+var NODE_MODULES_PATH = path.join('../../', 'node_modules');
 
-var swig = Swig(r, {
-    viewdir: __dirname + '/tpls'
+app.use(require(path.join(NODE_MODULES_PATH, '/yog/middleware/yog-resource-api'))({config_dir: __dirname + '/config'}));
+app.use(require('yog-bigpipe')());
+
+
+var layer = require(path.join(NODE_MODULES_PATH, '/yog-view/lib/layer.js'));
+var wrap = require('../../');
+
+describe('tags', function () {
+    var tests = {
+        '/html': {
+            'tpl': '{%html%}test{%endhtml%}',
+            'expect': '<html>test<!--FIS_BIGPIPE_HOOK--></html>'
+        },
+        '/body': {
+            'tpl': '{%body%}test{%endbody%}',
+            'expect': '<body>test<!--FIS_JS_HOOK--></body>'
+        },
+        '/style': {
+            'tpl': '{%style%}alert("test");{%endstyle%}',
+            'expect': ''
+        }
+    };
+
+    for (var key in tests) {
+        if (!tests.hasOwnProperty(key)) { continue; }
+        var itr = tests[key];
+        (function (key, itr) {
+            app.get(key, function (req, res) {
+                var options = {};
+                var protocol = layer(res.fis, res.bigpipe);
+                options.locals = {_yog: protocol};
+                var swig = wrap(options, protocol).swig;
+                res.end(swig.run(swig.compile(itr['tpl'])));
+            });
+
+            it(key, function (done) {
+                request(app)
+                    .get(key)
+                    .end(function (err, res) {
+                        expect(res.text).to.equal(itr['expect']);
+                        done();
+                    });
+            });
+        })(key, itr);
+    }
 });
-
-console.log(swig._render(__dirname + '/tpls/test01.tpl', {
-    obj_var: {
-        title: 'it\'s a widget'
-    }
-}, {
-    "widget01": function (done) {
-        fs.readFile(__dirname + '/data/pagelet01.json', function (err, json) {
-            if (err) {
-                throw err;
-            }
-            done(JSON.parse(json));
-        });
-    }
-}));
-console.log(r);
