dfccc86d4fcfa163a6d0604ae75bdf4448586d8a
xiangshouding <fansekey@gmail.com>
2014-07-08 19:17:48 +0800

    update ut

--- test/ut/test.js
+++ test/ut/test.js
@@ -4,28 +4,43 @@ var expect = require('chai').expect;
 var yog = require('yog');
 var app = express();
 var path = require('path');
-var NODE_MODULES_PATH = path.join('../../', 'node_modules');
 
-app.use(require(path.join(NODE_MODULES_PATH, '/yog/middleware/yog-resource-api'))({config_dir: __dirname + '/config'}));
-app.use(require('yog-bigpipe')());
+var NODE_MODULES_PATH = path.join('../../', 'node_modules');
+var rApi = require(path.join(NODE_MODULES_PATH, '/yog/middleware/yog-resource-api'));
+var bigpipe = require('yog-bigpipe');
+
+app.use(rApi({
+    config_dir: __dirname + '/config'
+}));
+
+app.use(bigpipe());
 
 
 var layer = require(path.join(NODE_MODULES_PATH, '/yog-view/lib/layer.js'));
+
+// require self
 var wrap = require('../../');
 
 describe('tags', function () {
     var tests = {
         '/html': {
             'tpl': '{%html%}test{%endhtml%}',
+            'isString': true,
             'expect': '<html>test<!--FIS_BIGPIPE_HOOK--></html>'
         },
         '/body': {
             'tpl': '{%body%}test{%endbody%}',
+            'isString': true,
             'expect': '<body>test<!--FIS_JS_HOOK--></body>'
         },
         '/style': {
             'tpl': '{%style%}alert("test");{%endstyle%}',
+            'isString': true,
             'expect': ''
+        },
+        '/html_index.tpl': {
+            'tpl': 'index.tpl',
+            'expect': '<html>test<!--FIS_BIGPIPE_HOOK--></html>'
         }
     };
 
@@ -34,11 +49,17 @@ describe('tags', function () {
         var itr = tests[key];
         (function (key, itr) {
             app.get(key, function (req, res) {
-                var options = {};
+                var options = {
+                    views: path.join(__dirname, '/tpls')
+                };
                 var protocol = layer(res.fis, res.bigpipe);
                 options.locals = {_yog: protocol};
                 var swig = wrap(options, protocol).swig;
-                res.end(swig.run(swig.compile(itr['tpl'])));
+                if (itr['isString']) {
+                    res.end(swig.run(swig.compile(itr['tpl'])));
+                } else {
+                    res.end(swig.renderFile(itr['tpl']));
+                }
             });
 
             it(key, function (done) {
--- test/ut/tpls/widget01.tpl
+++
@@ -1,4 +0,0 @@
-<div>
-<h3> {{title}} </h3>
-{%uri "ns:widget/widget01.tpl"%}
-</div>
